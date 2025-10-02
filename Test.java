ce82f00f4dc9198edc2c0970d953f45d39bd7857
Cindy Callista N <cindnathania@gmail.com>
2025-10-02 14:55:40 +0700

    menambah halloww ada text baru

--- Test.java
+++ Test.java
@@ -3,5 +3,6 @@ public class Test {
     public static void main(String [] args) {
         System.out.println("Hi, ini baris kode pertama");
         System.out.println("Halo, ini ninininini");
+        System.out.println("Hallowwww ada text baru nih");
     }
 }
