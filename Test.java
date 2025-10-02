dbb68413899a320ffb9f77badfbe55a7f8b3597f
Inas Asami El <inasasami11@gmail.com>
2025-10-02 15:00:07 +0700

    manambah dutbsdnanltee

--- Test.java
+++ Test.java
@@ -4,5 +4,6 @@ public class Test {
         System.out.println("Hi, ini baris kode pertama");
         System.out.println("Halo, ini ninininini");
         System.out.println("Hallowwww ada text baru nih");
+        System.out.println("lattetetetetet");
     }
 }
