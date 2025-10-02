40d815f035e1a0d99df612089756f476e865a25e
Cindy Callista N <cindnathania@gmail.com>
2025-10-02 14:17:07 +0700

    project baru

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 public class Test {
 
     public static void main(String [] args) {
-        System.out.prinln("Hi, ini baris kode pertama");
+        System.out.println("Hi, ini baris kode pertama");
     }
 }
